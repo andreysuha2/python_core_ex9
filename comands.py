c87cee7120f07f87ae81af3e43b1d493b65ba1fd
Andrii Sukhenko <andreysuha2@gmil.com>
2023-08-29 17:11:28 +0300

    fix handling errors, add help comand

--- comands.py
+++ comands.py
@@ -10,38 +10,44 @@ def input_error(handler):
         except ValueError:
             return "You are trying to set invalid value"
         except IndexError:
-            return "Something went wrong, try again"
+            return "You are sending invalid count of parameters. Please use help comand for hint"
     return inner
 
 @input_error
-def add(name, phone):
+def add(*args):
+    name, phone = args[0], args[1]
     if name in dictionary:
         return f'Contact with name {name} already exists.'
     dictionary[name] = phone
     return f'"{name}" added to conctacts with phone "{phone}".'
 
 @input_error
-def change(name, phone):
+def change(*args):
+    name, phone = args[0], args[1]
     if name in dictionary:
         dictionary[name] = phone
         return f'For contact "{name}" phone chaged to {phone}'
     return f'Contact with name "{name}" doesn\'t exist.'
 
 @input_error
-def phone(name):
+def phone(*args):
+    name = args[0]
     if name in dictionary:
         return f"{name}: {dictionary[name]}"
     return f'Contact with name "{name}" doesn\'t exist.'
 
 @input_error
-def remove(name):
+def remove(*args):
+    name = args[0]
     if name in dictionary:
         dictionary.pop(name)
         return f'Contact "{name}" was removed from contacts!'
     return f'Contact "{name}" doesn\'t exist.'
 
 @input_error
-def show_all():
+def show_all(*args):
+    if len(args):
+        raise IndexError
     output = "---CONTACTS---\n"
     if len(dictionary):
         for key, value in dictionary.items():
@@ -50,11 +56,37 @@ def show_all():
     else:
         output += "Contacts are empty"
         return output
+    
+@input_error    
+def help(*args):
+    return """
+        --- CONTACTS HELP ---
+        syntax: add {name} {phone}
+        description: adding number to contacts list 
+        example: add ivan +380999999999
+
+        syntax: change {name} {phone}
+        description: changing phone number for contact
+        example: change ivan +380999999999
+
+        syntax: phone {name}
+        description: finding phone number by contact name
+        example: phone ivan
+
+        syntax: remove {name}
+        description: removing contact from contacts list
+        example: remove ivan
+
+        syntax: show all
+        description: showing list of contacts
+        example: show all
+    """
 
 CLOSE_COMANDS = ("good bye", "close", "exit")
 HANDLERS = {
     "add": add,
     "change": change,
     "phone": phone,
-    "show all": show_all
+    "show all": show_all,
+    "help": help
 }
